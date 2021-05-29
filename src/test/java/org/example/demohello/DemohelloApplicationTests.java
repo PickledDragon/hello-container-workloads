25bd6acf61089ae06f0b350ad7e7d0aaea915cbe
Praveen Kottarathil <praveenkottarathil@gmail.com>
2021-05-29 22:23:11 +0200

    Initial version

---
+++ src/test/java/org/example/demohello/DemohelloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demohello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
